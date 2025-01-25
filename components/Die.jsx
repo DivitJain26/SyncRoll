150cf65d9e739e80219ca9a20e644f22fd9ce8b1
Divit Jain <divit26indore@gmail.com>
2025-01-25 15:42:37 +0530

    New Features

--- components/Die.jsx
+++ components/Die.jsx
@@ -1,8 +1,37 @@
 export default function Die(props) {
-    const Diecolor = {backgroundColor: props.isHeld ? "#59E391" : "#FFFF"}
+    let Die = {}
+
+    if (props.isHeld) {
+        if (props.correctValue === true) {
+            Die.backgroundColor = "#59E391"
+        } else {
+            Die.backgroundColor = "#F4010D"
+        }
+    } else {
+        Die.backgroundColor = "#FFFF"
+    }
+
+    if (window.innerWidth > 768) {
+        if (props.numberOfDice == 15) {
+            Die.height = "6vw"
+            Die.width = "6vw"
+            Die.fontSize = "2.57vw"
+        }
+        if (props.numberOfDice == 20) {
+            Die.height = "5vw"
+            Die.width = "5vw"
+            Die.fontSize = "2.14vw"
+        }
+        if (props.numberOfDice == 25) {
+            Die.height = "4.5vw"
+            Die.width = "4.55vw"
+            Die.fontSize = "1.92vw"
+        }
+    }
+
     return (
         <button 
-            style={Diecolor} 
+            style={Die} 
             onClick={props.hold}
             aria-pressed={props.isHeld}
             aria-label={`Die with value ${props.value}, 
--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,21 @@ import Die from "../components/Die"
 
 export default function App() {
 
-    const [dice, setDice] = React.useState(() => generateAllNewDice())
-// modified
+    const [dice, setDice] = React.useState(() => generateAllNewDice(10))
+    const [isGameOn, setIsGameOn] = React.useState(false)
+    const [firstHeldValue, setFirstHeldValue] = React.useState(null)
+    const [gameSettings, setGameSettings] = React.useState({
+        interval: 1000,
+        numberOfDice: 10,
+    })
+    const [isGameOver, setIsGameOver] = React.useState(false)
+
     const buttonRef = React.useRef(null)
-    
-    const gameWon =
-        dice.every(die => die.isHeld) &&
-        dice.every(die => die.value === dice[0].value)
-        
-    React.useEffect(() => {
-        if (gameWon) {
-            buttonRef.current.focus()
-        }
-    }, [gameWon])
- 
-    function generateAllNewDice() {
-        return new Array(10)
-            .fill(0)
-            .map(() => ({
-                value: Math.ceil(Math.random() * 6),
-                isHeld: false,
-                id: nanoid()
-            }))
-    }
-    
-    // console.log(dice)
+    const intervalRef = React.useRef(null)
 
-    
-    function rollDice() {
-        gameWon 
-        ? setDice(generateAllNewDice())
-        : setDice(prevDie => prevDie.map(die => {
-            return !die.isHeld ? {...die, value: Math.ceil(Math.random() * 6)} : die 
-        }))
-    }
-
-    function hold(id) {
-        setDice(prevDie => prevDie.map(die => {
-            return die.id === id ? { ...die, isHeld: !die.isHeld } : die
-        }))
-    }
+    var click = new Audio("assets/click2.mp3")
+    var wrong = new Audio("assets/wrong.mp3")
+    var win = new Audio("assets/the_final_countdown.mp3")
 
     const dicesElement = dice.map(dieObj =>
         <Die
@@ -52,30 +27,156 @@ export default function App() {
             value={dieObj.value}
             isHeld={dieObj.isHeld}
             hold={() => hold(dieObj.id)}
+            correctValue={dieObj.correctValue}
+            numberOfDice={gameSettings.numberOfDice}
         />)
+
+    const gameWon =
+        dice.every(die => die.isHeld) &&
+        dice.every(die => die.value === dice[0].value)
+
+    let dieContainer = {}
+    if (gameSettings.numberOfDice >= 20 && window.innerWidth > 768) {
+        dieContainer.gap = "1.28vw"
+        dieContainer.marginBottom = "4%"
+    }
+
+    React.useEffect(() => {
+        if (gameWon) {
+            buttonRef.current.focus()
+        }
+    }, [gameWon])
+
+    // Cleanup the interval when the component unmounts
+    React.useEffect(() => {
+        return () => clearInterval(intervalRef.current);
+    }, [])
+
+    React.useEffect(() => {
+        setDice(generateAllNewDice(gameSettings.numberOfDice));
+        setFirstHeldValue(null);
+        setIsGameOn(false);
+        setIsGameOver(false);
+    }, [gameSettings.numberOfDice]);
+
+    function generateAllNewDice(num) {
+        return new Array(num)
+            .fill(0)
+            .map(() => ({
+                value: Math.ceil(Math.random() * 6),
+                isHeld: false,
+                correctValue: true,
+                id: nanoid()
+            }))
+    }
+
+    function roll() {
+        setDice(prevDie => prevDie.map(die => {
+            return !die.isHeld ? { ...die, value: Math.ceil(Math.random() * 6) } : die
+        }))
+    }
+
+    function toggleGame() {
+        setIsGameOn(prev => {
+            const newGameState = !prev;
+
+            clearInterval(intervalRef.current);
+            // setDice(generateAllNewDice())
+            setDice(generateAllNewDice(gameSettings.numberOfDice))
+            setFirstHeldValue(null)
+            setIsGameOver(false)
+
+            if (newGameState) {
+                intervalRef.current = setInterval(roll, gameSettings.interval);
+            }
+
+            return newGameState;
+        })
+    }
+
+    function hold(id) {
+        if (!isGameOn || isGameOver) return;
+
+        setDice(prevDie => prevDie.map(die => {
+            if (die.id === id) {
+                if (!die.isHeld && firstHeldValue === null) {
+                    click.play()
+                    setFirstHeldValue(die.value)
+                }
+                if (die.value === firstHeldValue || firstHeldValue === null) {
+                    click.play()
+                    return { ...die, isHeld: true }
+                } else {
+                    wrong.play()
+                    clearInterval(intervalRef.current);
+                    setIsGameOver(true);
+                    return { ...die, isHeld: true, correctValue: false }
+                }
+            } else {
+                return die
+            }
+        }))
+    }
+
+    function handleChange(event) {
+        const { value, name } = event.target
+        setGameSettings(prevSettings => ({ ...prevSettings, [name]: Number(value) }))
+    }
 
     return (
         <main>
-            
             {gameWon && <Confetti
-                width={window.innerWidth } 
-                height={window.innerHeight}/>
+                width={window.innerWidth}
+                height={window.innerHeight} /> 
             }
+            
             <div aria-live="polite" className="sr-only">
                 {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
             </div>
-            <h1 className="title">Tenzies</h1>
-            <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-            <div className="dice-container">
-                {dicesElement}
+            <h1 className="title">SyncRoll</h1>
+            <p className="instructions">Click on rolling dice to freeze them. Make all dice show the same value to win!</p>
+
+            <div className="dropdown-div">
+                <label htmlFor="interval">Mode</label>
+
+                <label htmlFor="numberOfDice">Dice</label>
+
+                <select
+                    name="interval"
+                    value={gameSettings.interval}
+                    onChange={handleChange}
+                    disabled={isGameOn ? true : false}
+                >
+                    <option value={1500}>Easy</option>
+                    <option value={1000}>Medium</option>
+                    <option value={600}>Hard</option>
+                    <option value={100}>Nightmare</option>
+                </select>
+
+                <select
+                    name="numberOfDice"
+                    value={gameSettings.numberOfDice}
+                    onChange={handleChange}
+                    disabled={isGameOn ? true : false}
+                >
+                    <option value={10}>10</option>
+                    <option value={15}>15</option>
+                    <option value={20}>20</option>
+                    <option value={25}>25</option>
+                </select>
             </div>
-            <button 
-                className="roll-dice" 
-                onClick={rollDice} 
+
+            <div 
+                style={dieContainer}
+                className="dice-container"
+            >{dicesElement} 
+            </div>
+
+            <button
+                className="roll-dice"
+                onClick={toggleGame}
                 ref={buttonRef}
-            >{gameWon ? "New Game" : "Roll"}</button>
+            >{(isGameOver || !isGameOn) ? "New Game" : "Start"}</button>
         </main>
     )
 }
-
-
